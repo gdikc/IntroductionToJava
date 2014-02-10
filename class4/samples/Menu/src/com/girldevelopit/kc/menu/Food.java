cef2c207d409db73c9eecb9106f870e774e3ebfa
Michelle Brush <michelle.brush@gmail.com>
2014-02-09 19:04:51 -0600

    Remove unnecessary methods

--- class4/samples/Menu/src/com/girldevelopit/kc/menu/Food.java
+++ class4/samples/Menu/src/com/girldevelopit/kc/menu/Food.java
@@ -23,23 +23,4 @@ public class Food {
         return description;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Food food = (Food) o;
-
-        if (description != null ? !description.equals(food.description) : food.description != null) return false;
-         if (name != null ? !name.equals(food.name) : food.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        return result;
-    }
 }
